6c7081ade5476f6409dbe98e1c5c15610475f8af
nikolay-igorev <frololo.frololo@yandex.ru>
2024-12-14 15:58:16 +0700

    fix: search

--- search-assistant.py
+++ search-assistant.py
@@ -14,19 +14,25 @@ def main() -> None:
     sdk = YCloudML(folder_id="<идентификатор_каталога>", auth="<API-ключ>")
 
     start_connection_assistant = time.time()
-    assistant = sdk.assistants.get('<Assistant ID>')
+    assistant = sdk.assistants.get('fvtd1b90ftt1sibhvcjq')
     time_connection_assistant = time.time() - start_connection_assistant
+    time_first_text_assistant = 1000
 
     thread = sdk.threads.create()
 
-    thread.write("Что такое Казахстан?")
+    thread.write("Расскажи всё, что знаешь про Бали. Важно очень много текста.")
     run = assistant.run_stream(thread)
+    i = 0
     for event in run:
         os.system('cls')
+        if i == 0:
+            time_first_text_assistant = time.time() - start_connection_assistant
+        i += 1
         print(f'{event.text}')
-        time.sleep(0.5)
+        time.sleep(0.01)
 
     print(f'Время подключения к ассистенту: {time_connection_assistant}')
+    print(f'Время до получения первой части текста": {time_first_text_assistant}')
 
     thread.delete()
 
